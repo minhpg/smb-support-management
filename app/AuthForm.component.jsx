95b62c73aea509fa57f836b3acd4e2be4baca536
minhpg <minhpg@gmail.com>
2024-01-23 09:42:28 +0700

    fix: vercel url

--- app/AuthForm.component.jsx
+++ app/AuthForm.component.jsx
@@ -20,7 +20,7 @@ const AuthForm = () => {
             theme="light"
             showLinks={false}
             providers={[]}
-            redirectTo="http://localhost:3000/auth/callback"
+            redirectTo={`${NEXT_PUBLIC_APP_URL}/auth/callback`}
           />
         </div>
       </div>
